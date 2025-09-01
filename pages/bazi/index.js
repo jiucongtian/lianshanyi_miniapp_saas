382b7ced10d01270703730d105758ac4ae2b3670
zhenghao <zhenghao1@gwm.cn>
2025-09-02 07:33:19 +0800

    在八字页面中引入八字图片映射表，优化图片路径设置逻辑；更新年、月、日、时柱的图片路径，确保页面渲染时正确显示对应的八字图片，提升用户体验。

--- pages/bazi/index.js
+++ pages/bazi/index.js
@@ -1,3 +1,6 @@
+// 引入八字图片映射表 [[memory:7739041]]
+const { getBaziImageById, getBaziImageByPinyin } = require('../../utils/baziImageMap');
+
 Page({
   data: {
     deviceSize: 'medium',
@@ -5,22 +8,22 @@ Page({
     yearPillar: { 
       heavenlyStem: '甲',
       earthlyBranch: '子',
-      imageName: '甲子.jpg'
+      imagePath: ''  // 将在onReady中设置
     },
     monthPillar: {
-      heavenlyStem: '甲',
-      earthlyBranch: '子',
-      imageName: '甲子.jpg'
+      heavenlyStem: '乙',
+      earthlyBranch: '丑',
+      imagePath: ''  // 将在onReady中设置
     },
     dayPillar: {
-      heavenlyStem: '甲',
-      earthlyBranch: '子',
-      imageName: '甲子.jpg'
+      heavenlyStem: '丙',
+      earthlyBranch: '寅',
+      imagePath: ''  // 将在onReady中设置
     },
     timePillar: {
-      heavenlyStem: '甲',
-      earthlyBranch: '子',
-      imageName: '甲子.jpg'
+      heavenlyStem: '丁',
+      earthlyBranch: '卯',
+      imagePath: ''  // 将在onReady中设置
     },
     originalTime: '',
     lunarTime: '',
@@ -35,6 +38,25 @@ Page({
     // 处理传递过来的参数
     this.handleReceivedParams(options);
   },
+
+  onReady: function() {
+    // 页面渲染完成后，根据天干地支设置正确的图片路径
+    this.updateInitialImages();
+  },
+
+  // 初始化时更新图片路径
+  updateInitialImages: function() {
+    const { yearPillar, monthPillar, dayPillar, timePillar } = this.data;
+    
+    this.setData({
+      'yearPillar.imagePath': this.getBaziImagePath(yearPillar.heavenlyStem, yearPillar.earthlyBranch),
+      'monthPillar.imagePath': this.getBaziImagePath(monthPillar.heavenlyStem, monthPillar.earthlyBranch),
+      'dayPillar.imagePath': this.getBaziImagePath(dayPillar.heavenlyStem, dayPillar.earthlyBranch),
+      'timePillar.imagePath': this.getBaziImagePath(timePillar.heavenlyStem, timePillar.earthlyBranch)
+    });
+  },
+
+
 
   // 处理接收到的参数
   handleReceivedParams: function(options) {
@@ -111,21 +133,53 @@ Page({
     this.setData({ showTimePopup: e.detail.visible });
   },
 
+  // 根据天干地支获取对应的图片路径
+  getBaziImagePath: function(heavenlyStem, earthlyBranch) {
+    // 将天干地支转换为拼音
+    const tianGanMap = {
+      '甲': 'jia', '乙': 'yi', '丙': 'bing', '丁': 'ding', '戊': 'wu',
+      '己': 'ji', '庚': 'geng', '辛': 'xin', '壬': 'ren', '癸': 'gui'
+    };
+    
+    const diZhiMap = {
+      '子': 'zi', '丑': 'chou', '寅': 'yin', '卯': 'mao', '辰': 'chen', '巳': 'si',
+      '午': 'wu', '未': 'wei', '申': 'shen', '酉': 'you', '戌': 'xu', '亥': 'hai'
+    };
+    
+    const tianGanPinyin = tianGanMap[heavenlyStem];
+    const diZhiPinyin = diZhiMap[earthlyBranch];
+    
+    if (tianGanPinyin && diZhiPinyin) {
+      const pinyin = tianGanPinyin + diZhiPinyin;
+      const imageInfo = getBaziImageByPinyin(pinyin);
+      return imageInfo ? imageInfo.imagePath : '/static/new_bazi/01_jiazi.png';
+    }
+    
+    return '/static/new_bazi/01_jiazi.png'; // 默认图片
+  },
+
   // 更新八字图片
   updateBaziImages: function(data) {
-    // 这里应该根据实际的天干地支组合来设置正确的图片名称
     this.setData({
       yearPillar: { 
-        imageName: `${data.yearPillar.heavenlyStem}${data.yearPillar.earthlyBranch}.JPG`
+        heavenlyStem: data.yearPillar.heavenlyStem,
+        earthlyBranch: data.yearPillar.earthlyBranch,
+        imagePath: this.getBaziImagePath(data.yearPillar.heavenlyStem, data.yearPillar.earthlyBranch)
       },
       monthPillar: {
-        imageName: `${data.monthPillar.heavenlyStem}${data.monthPillar.earthlyBranch}.JPG`
+        heavenlyStem: data.monthPillar.heavenlyStem,
+        earthlyBranch: data.monthPillar.earthlyBranch,
+        imagePath: this.getBaziImagePath(data.monthPillar.heavenlyStem, data.monthPillar.earthlyBranch)
       },
       dayPillar: {
-        imageName: `${data.dayPillar.heavenlyStem}${data.dayPillar.earthlyBranch}.JPG`
+        heavenlyStem: data.dayPillar.heavenlyStem,
+        earthlyBranch: data.dayPillar.earthlyBranch,
+        imagePath: this.getBaziImagePath(data.dayPillar.heavenlyStem, data.dayPillar.earthlyBranch)
       },
       timePillar: {
-        imageName: `${data.timePillar.heavenlyStem}${data.timePillar.earthlyBranch}.JPG`
+        heavenlyStem: data.timePillar.heavenlyStem,
+        earthlyBranch: data.timePillar.earthlyBranch,
+        imagePath: this.getBaziImagePath(data.timePillar.heavenlyStem, data.timePillar.earthlyBranch)
       },
       originalTime: data.originalTime,
       lunarTime: data.lunarTime
