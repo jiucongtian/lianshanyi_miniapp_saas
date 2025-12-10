a65dd2a12478273d54bd43858626b356cae554a1
郑浩 <gw00241153@tech.gwm.cn>
2025-12-10 15:23:50 +0800

    feat: 更新卡牌图片存储路径和文件名格式
    - 修改配置文件中的卡牌图片云存储路径为 'cards_v2'
    - 更新生成卡牌图片映射的文件名格式，改为使用 '.jpg' 后缀，提升一致性和兼容性

--- miniprogram/utils/baziImageMap.js
+++ miniprogram/utils/baziImageMap.js
@@ -73,7 +73,8 @@ const generateBaziImageMap = () => {
 
   // 生成完整的映射表，包含图片路径
   return baziData.map(item => {
-    const fileName = `${item.num.toString().padStart(2, '0')}_${item.pinyin}.png`;
+    // 新格式：编号 + .jpg，例如 "01.jpg"
+    const fileName = `${item.num.toString().padStart(2, '0')}.jpg`;
     
     // 使用云存储路径
     // 云存储文件路径格式：cloud://环境ID.云存储桶ID/路径/文件名
